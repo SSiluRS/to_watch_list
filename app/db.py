3a2ac4459f2e8f8fe278fe81bd17e8f19fae673f
SSiluRS <Endercresman397@mail.ru>
2025-08-13 22:13:46 +0700

    fix db connection

--- app/db.py
+++ app/db.py
@@ -2,15 +2,19 @@ import os
 import mysql.connector
 from mysql.connector import pooling
 
+# Конфигурация берётся из .env
 DB_CFG = dict(
-    host=os.getenv("MYSQL_HOST", "db"),
-    user=os.getenv("MYSQL_USER", "to_watch_list"),
-    password=os.getenv("MYSQL_PASSWORD", "pass"),
-    database=os.getenv("MYSQL_DATABASE", "to_watch_list"),
+    host=os.getenv("MYSQL_HOST", ""),  # IP хоста, где MariaDB
+    port=int(os.getenv("MYSQL_PORT", 3306)),
+    user=os.getenv("MYSQL_USER", ""),
+    password=os.getenv("MYSQL_PASSWORD", ""),
+    database=os.getenv("MYSQL_DATABASE", ""),
     charset="utf8mb4",
 )
 
+# Пул соединений
 cnxpool = pooling.MySQLConnectionPool(pool_name="twl_pool", pool_size=5, **DB_CFG)
 
 def get_conn():
+    """Получить соединение с БД из пула"""
     return cnxpool.get_connection()
